b951226bc9c01896ec2b5bca0b3be0cbe6b0fd1c
Joshua Gohr <jgohwork@gmail.com>
2022-10-10 14:05:21 -0400

    Styles for follow prompt

--- comps/FollowPrompt.jsx
+++ comps/FollowPrompt.jsx
@@ -3,12 +3,16 @@ import styles from './FollowPrompt.module.css';
 
 function FollowPrompt() {
     return(<div className={styles.container}>
-        <h1>Enjoy The Content?</h1>
-        <h2>Consider Following The Blog!</h2>
+        <h1>Enjoy the content?</h1>
+        <h2>Consider following the blog!</h2>
+        <p>Following simply enrolls you to receieve updates on new posts. No spam & the 
+            option to unsubscribe whenever you please.
+        </p>
         <form className={styles.formPrompt}>
             <p className='color-span'>Email</p>
-            <input type='email' name='email' />
-            <input type='submit' value='Follow' />
+            <input className={styles.promptInput} type='email' name='email'
+            placeholder='purasdev@email.com' />
+            <input className={styles.promptSubmit} type='submit' value='Follow' />
         </form>
     </div>)
 };
